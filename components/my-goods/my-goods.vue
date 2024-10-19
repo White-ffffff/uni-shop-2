8924d58694c45c765ed259e42f534e83410fea74
White <2629548240@qq.com>
2024-10-19 16:16:23 +0800

    完成了商品列表页面的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="goods-item">
+	  <!-- 左侧区域 -->
+	  <view class="goods-item-left">
+	    <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+	  </view>
+	  <!-- 右侧区域 -->
+	  <view class="goods-item-right">
+	    <!-- 商品的名字 -->
+	    <view class="goods-name">{{goods.goods_name}}</view>
+	    <view class="goods-info-box">
+        <!-- 在渲染商品价格的时候，通过管道符 | 调用过滤器 -->
+	      <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+	    </view>
+	  </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+				
+			};
+		},
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      }
+    },
+    filters: {
+      // 把数字处理为带两位小数点的数字
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+	}
+</script>
+
+<style lang="scss">
+.goods-item {
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+
+  .goods-item-left {
+    margin-right: 5px;
+
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    .goods-name {
+      font-size: 13px;
+    }
+
+    .goods-price {
+      font-size: 16px;
+      color: #c00000;
+    }
+  }
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		Goods_list 
+		<view class="goods-list">
+      <view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+        <my-goods :goods="item"></my-goods>
+      </view>
+    </view> 
+    
+    
 	</view>
 </template>
 
@@ -8,9 +14,80 @@
 	export default {
 		data() {
 			return {
-				
+        //定义请求参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示的数据
+          pagesize: 10,
+        },
+        // 商品列表
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0,
+        // 节流阀,是否正在请求数据
+        isloading: false
+         
 			};
-		}
+		},
+    onLoad(options) {
+      // 将页面参数转存到 this.queryObj 对象中
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      console.log(this.queryObj)
+      
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表数据的方法
+      async getGoodsList(cb) {
+        
+        // 打开节流阀
+        this.isloading = true
+        // 发起请求
+        const { data: res } = await uni.$http.get('api/public/v1/goods/search', this.queryObj)
+        // 关闭节流阀
+        this.isloading = false
+        // 数据请求完毕，就立即按需调用cb回调函数
+        cb && cb()
+        // 判断请求是否成功
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值:拓展运算符合并新旧数组
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      // 点击跳转到商品详情页面
+      gotoDetail(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
+    },
+    // 触底事件,实现上拉加载更多,重新获取数据列表
+    onReachBottom() {
+      // 判断是否还有下一页数据,当前的页码值 * 每页显示多少条数据 >= 总数条数
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕!!!')
+      // 判断是否正在请求其他数据，如果是，则不发起额外的请求
+      if (this.isloading) return
+      // 页码值自增＋1
+      this.queryObj.pagenum  += 1
+      // 重新获取数组
+      this.getGoodsList()
+    },
+    // 下拉刷新的事件
+    onPullDownRefresh() {
+      // 1.重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+      // 2.重新发起请求,传入回调函数
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    }
 	}
 </script>
 
